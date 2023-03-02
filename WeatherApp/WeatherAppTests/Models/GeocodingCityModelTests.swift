93c2011c9fecdb52dd1eecfa52f674cb8e27ea5c
Fiseha Gezahegn <fiseha.gezahegn@jpmchase.com>
2023-03-02 00:37:05 -0600

    Add base test case support files and GeocodingCityModel tetsts.

---
+++ WeatherApp/WeatherAppTests/Models/GeocodingCityModelTests.swift
@@ -0,0 +1,28 @@
+//
+//  GeocodingCityModelTests.swift
+//  WeatherAppTests
+//
+//  Created by Fiseha Gezahegn on 3/2/23.
+//
+
+import XCTest
+@testable import WeatherApp
+
+final class GeocodingCityModelTests: WeatherAppTestCase {
+    
+    func testModelDoesParseWhenValidJSON() {
+        // Given, When
+        let response = parse("GeocodingAPI", toType: [GeocodingCityModel].self)
+        // Then
+        XCTAssertEqual(response?.first?.lon, -119.4432)
+        XCTAssertEqual(response?.first?.lat, 36.4761)
+    }
+    
+    
+    func testModelDoesNotParseWhenInvalidJSON() {
+        // Given, When
+        let response = parse("InvalidGeocodingAPI", toType: [GeocodingCityModel].self)
+        // Then
+        XCTAssertNil(response)
+    }
+}
---
+++ WeatherApp/WeatherAppTests/Resources/WeatherAppTestCase.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherAppTestCase.swift
+//  WeatherAppTests
+//
+//  Created by Fiseha Gezahegn on 3/2/23.
+//
+
+import XCTest
+
+class WeatherAppTestCase: XCTestCase {
+
+    func parse<T: Codable>(_ filename: String, toType dataType: T.Type) -> T? {
+        if let url = Bundle(for: type(of: self)).url(forResource: filename, withExtension: "json"),
+           let data = try? Data(contentsOf: url),
+           let output = try? JSONDecoder().decode(dataType.self, from: data) {
+            return output
+        }
+        return nil
+    }
+}
